6f91b635984c2b8b3832d184ed7549984c82a6ac
Scott Chase Waggener <tidalpaladin@protonmail.com>
2023-03-15 16:03:32 -0500

    Fix warnings

--- tests/test_tasks/test_task.py
+++ tests/test_tasks/test_task.py
@@ -40,7 +40,7 @@ class TestTask:
     )
     def test_run(self, task, default_root_dir, accelerator, datamodule, stage, named_datasets):
         if accelerator == "gpu":
-            precision = "bf16" if torch.cuda.get_device_capability(0)[0] >= 8 else "16"
+            precision = "bf16-mixed" if torch.cuda.get_device_capability(0)[0] >= 8 else "16"
         else:
             precision = 32
 
@@ -67,7 +67,7 @@ class TestTask:
     )
     def test_cli(self, mocker, tmp_path, task, datamodule, default_root_dir, accelerator, stage):
         if accelerator == "gpu":
-            precision = "bf16" if torch.cuda.get_device_capability(0)[0] >= 8 else "16"
+            precision = "bf16-mixed" if torch.cuda.get_device_capability(0)[0] >= 8 else "16"
         else:
             precision = 32
 
