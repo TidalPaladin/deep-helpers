65ae7eb7334cc0c52ea772142f6cdcbbe1ca495c
Scott Chase Waggener <tidalpaladin@protonmail.com>
2023-10-06 09:34:20 -0500

    Report stats on successfully loaded weights Fixes #40

--- deep_helpers/helpers.py
+++ deep_helpers/helpers.py
@@ -6,12 +6,13 @@ from typing import Any, Dict, cast
 
 import torch
 import torch.nn as nn
-from pytorch_lightning.utilities.rank_zero import rank_zero_only  # type: ignore
+from lightning_utilities.core.rank_zero import rank_zero_info
 
 
 def load_checkpoint(model: nn.Module, state_dict: Dict[str, Any], strict: bool = True) -> nn.Module:
     """Load weights from a state dict. When loading with ``strict=False``, missing or unexpected
-    keys will be ignored and keys with shapes that do not match will be ignored.
+    keys will be ignored and keys with shapes that do not match will be ignored. The rank zero
+    process will log information about what layers from `model` were loaded from `state_dict`.
 
     Args:
         model: The model to load weights into.
@@ -28,6 +29,10 @@ def load_checkpoint(model: nn.Module, state_dict: Dict[str, Any], strict: bool =
         checkpoint_state_dict = {
             k: v for k, v in state_dict.items() if k in model_state_dict and v.shape == model_state_dict[k].shape
         }
+        loaded_layers = len(checkpoint_state_dict)
+        total_layers = len(model_state_dict)
+        loaded_percent = loaded_layers / max(total_layers, 1) * 100
+        rank_zero_info(f"Loaded {loaded_layers} out of {total_layers} ({loaded_percent:.1f}%) layers from checkpoint.")
         model.load_state_dict(checkpoint_state_dict, strict=False)
     return model
 
